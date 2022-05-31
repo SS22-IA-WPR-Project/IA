c75c3e1bbdfb7b16da14b80939d4b0a20c660e2b
LNerking <lars.nerking@smail.th-koeln.de>
2022-05-31 20:28:21 +0200

    test
    test new unity

--- 3D_Study_Rooms_IA/Assets/Code/SpawnManager.cs
+++ 3D_Study_Rooms_IA/Assets/Code/SpawnManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Test für new Unity
 namespace Schlaffner_Andre {
 	public class SpawnManager : MonoBehaviour
 	{
