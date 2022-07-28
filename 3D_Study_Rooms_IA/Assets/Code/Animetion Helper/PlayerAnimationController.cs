895605d66616d09bccaaad3e439b989d2096cf46
LNerking <lars.nerking@smail.th-koeln.de>
2022-07-28 23:36:10 +0200

    bewegungen

--- 3D_Study_Rooms_IA/Assets/Code/Animetion Helper/PlayerAnimationController.cs
+++ 3D_Study_Rooms_IA/Assets/Code/Animetion Helper/PlayerAnimationController.cs
@@ -10,7 +10,9 @@ namespace Studyrooms
 
         Animator animator;
         int isWalkingHash;
-
+        int isWalkingBackwardsHash;
+        int isWalkingRightHash;
+        int isWalkingLeftHash;
 
 
         // Start is called before the first frame update
@@ -18,6 +20,9 @@ namespace Studyrooms
         {
             animator = GetComponent<Animator>();
             isWalkingHash = Animator.StringToHash("isWalking");
+            isWalkingBackwardsHash = Animator.StringToHash("isWalkingBackwards");
+            isWalkingRightHash = Animator.StringToHash("isWalkingRight");
+            isWalkingLeftHash = Animator.StringToHash("isWalkingLeft");
         }
 
         // Update is called once per frame
@@ -25,18 +30,59 @@ namespace Studyrooms
         {
 
             bool isWalking = animator.GetBool(isWalkingHash);
+            bool isWalkingBackward = animator.GetBool(isWalkingBackwardsHash);
+            bool isWalkingRight = animator.GetBool(isWalkingRightHash);
+            bool isWalkingLeft = animator.GetBool(isWalkingLeftHash);
+
             bool forwardPressed = (Input.GetKey("up") || Input.GetKey("w"));
+            bool backwardPressed = (Input.GetKey("down") || Input.GetKey("s"));
+            bool rightPressed = (Input.GetKey("right") || Input.GetKey("d"));
+            bool leftPressed = (Input.GetKey("left") || Input.GetKey("a"));
+
             //Player animation
 
+            //forward
             if (!isWalking && forwardPressed)
             {
                 animator.SetBool("isWalking", true);
-
             }
 
             if (isWalking && !forwardPressed)
             {
                 animator.SetBool("isWalking", false);
+            }
+
+            //backward
+            if (!isWalkingBackward && backwardPressed)
+            {
+                animator.SetBool("isWalkingBackwards", true);
+            }
+
+            if (isWalkingBackward && !backwardPressed)
+            {
+                animator.SetBool("isWalkingBackwards", false);
+            }
+
+            //right
+            if (!isWalkingRight && rightPressed)
+            {
+                animator.SetBool("isWalkingRight", true);
+            }
+
+            if (isWalkingRight && ! rightPressed)
+            {
+                animator.SetBool("isWalkingRight", false);
+            }
+
+            //left
+            if ( !isWalkingLeft && leftPressed)
+            {
+                animator.SetBool("isWalkingLeft", true);
+            }
+
+            if ( isWalkingLeft && !leftPressed)
+            {
+                animator.SetBool("isWalkingLeft", false);
             }
         }
 
