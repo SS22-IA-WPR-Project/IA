0546887c18415c4635c687e6500580768b5bc908
CharlySkai <102866117+CharlySkai@users.noreply.github.com>
2022-08-18 19:37:21 +0200

    Update Event

--- 3D_Study_Rooms_IA/Assets/Code/EventSystem/Event.cs
+++ 3D_Study_Rooms_IA/Assets/Code/EventSystem/Event.cs
@@ -5,16 +5,25 @@ namespace Studyrooms
     public class Event
     {
         private event Action actionHere = delegate { };
+        private String i = "";
 
         public void Invoke()
         {
             actionHere?.Invoke();
         }
 
-        public void AddListener(Action data)
+        public void Invoke(int i)
+        {
+
+            actionHere?.Invoke();
+        }
+
+        public String AddListener(Action data)
         {
             actionHere += data;
+            return i;
         }
+
 
         public void RemoveListener(Action data)
         {
